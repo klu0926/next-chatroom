d3750a7815a58615bbe5d607f80afa518e035aac
klu0926 <lukuoyu@gmail.com>
2025-06-01 13:23:03 -0400

    use next.js Head instead of stand html head

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono,  } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
       <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
       <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-title" content="Chatterly" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      </head>
+      </Head>
       <body
         className={`bg-pink-50 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
